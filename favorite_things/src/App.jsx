3680fe0a75d6d931339c3ba268225c432d45a146
aperegri <aperegri@nd.edu>
2026-07-02 07:22:14 +0800

    Setup Vite and parse, added parse initialization

--- favorite_things/src/App.jsx
+++ favorite_things/src/App.jsx
@@ -1,5 +1,11 @@
+import Parse from 'parse'
 import './App.css'
 import Components from '../Components/Components.jsx'
+
+//connecting parse
+Parse.initialize("Esk2GTeZgctXdkVgpCFTzuE3epbEGmeSnh421YVE", "FOtU9JHQ5RfUybpIe7AjTYBsLe6Vhk0uTuYHsOgR")
+Parse.serverURL = 'https://parseapi.back4app.com/'
+
 
 export default function App() {
   return <Components />;
